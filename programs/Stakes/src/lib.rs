7abab2d3edba21b9f8dcc5fd6eac1baa453f6ffc
Nasri Adzlani <nasri@jkt1.ebdesk.com>
2022-06-15 05:14:59 +0700

    test

--- programs/Stakes/src/lib.rs
+++ programs/Stakes/src/lib.rs
@@ -1,15 +1,29 @@
 use anchor_lang::prelude::*;
-
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod stakes {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx:Context<Create>) -> ProgramResult{
+        let create = &mut ctx.accounts.stake_program;
+        create.amount = 0;
         Ok(())
     }
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info>{
+    #[account(init, payer=user, space=16+16)]
+    pub stake_program : Account<'info,StakeStruct>,
+    #[account(mut)]
+    pub user : Signer<'info>,
+    pub system_program : Program<'info, System>
+}
+#[account]
+pub struct StakeStruct{
+    pub amount : u64,
+}
--- tests/Stakes.ts
+++ tests/Stakes.ts
@@ -1,16 +1,27 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { Stakes } from "../target/types/stakes";
-
 describe("Stakes", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const { SystemProgram } = anchor.web3;
+  const keypair = anchor.web3.Keypair.generate();
   const program = anchor.workspace.Stakes as Program<Stakes>;
 
-  it("Is initialized!", async () => {
+  it("Is Create Program!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    await program.rpc.create({
+      accounts: {
+        stakeProgram: keypair.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [keypair],
+    });
+
+    const fetching = await program.account.stakeStruct.fetch(keypair.publicKey);
+    assert.ok(fetching.amount.eq(new anchor.BN(0)));
   });
 });
